0dacefd114a4690d4459e573e9ae09d851ba19ae
itsbadr <68023752+clickykeyboard@users.noreply.github.com>
2022-03-11 22:29:29 +0500

    delete active vc function

--- utils/vc.ts
+++ utils/vc.ts
@@ -9,6 +9,15 @@ export async function addActiveVoiceChannel(voiceChannelId: string): Promise<voi
   await client.disconnect()
 }
 
+export async function deleteActiveVoiceChannel(voiceChannelId: string | any): Promise<void> {
+  const client = createClient();
+  await client.connect();
+
+  await client.sRem("voice_channels", voiceChannelId);
+
+  await client.disconnect()
+}
+
 export async function getActiveVoiceChannels(): Promise<Array<string>> {
   const client = createClient();
   await client.connect();
@@ -20,6 +29,6 @@ export async function getActiveVoiceChannels(): Promise<Array<string>> {
   return voiceChannels;
 }
 
-export async function isVoiceChannelActive(voiceChannelId: string): Promise<boolean> {
+export async function isVoiceChannelActive(voiceChannelId: string | any): Promise<boolean> {
   return (await getActiveVoiceChannels()).includes(voiceChannelId);
 }
