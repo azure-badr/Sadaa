c208ce064b5bccb267313ff1934826b47bb17ff7
itsbadr <68023752+clickykeyboard@users.noreply.github.com>
2022-03-13 23:21:19 +0500

    add default perms to vc owner

--- events/join.ts
+++ events/join.ts
@@ -1,10 +1,11 @@
 import { CategoryChannel, VoiceState } from "discord.js"
 import { ChannelTypes } from "discord.js/typings/enums";
 
-import { voiceChannelId, categoryId, defaultUserLimit, defaultBitrate } from "../config.json";
+import { applicationId, voiceChannelId, categoryId, defaultUserLimit, defaultBitrate } from "../config.json";
 import { addActiveVoiceChannel } from "../utils/vc";
 
 async function createVoiceChannel(voiceState: VoiceState) {
+  const client = voiceState.client;
   const guild = voiceState.guild;
   const member = voiceState.member;
 
@@ -14,7 +15,27 @@ async function createVoiceChannel(voiceState: VoiceState) {
     const memberChannel = await channelCategory.createChannel(member?.displayName || "Channel", {
       type: ChannelTypes.GUILD_VOICE,
       bitrate: defaultBitrate,
-      userLimit: defaultUserLimit
+      userLimit: defaultUserLimit,
+      permissionOverwrites: [
+        // Overwrite for client
+        {
+          id: applicationId,
+          allow: ["VIEW_CHANNEL"],
+        },
+        // Overwrite for user
+        {
+          id: member?.id || '',
+          allow: [
+            "MANAGE_CHANNELS",
+            "VIEW_CHANNEL",
+            "CONNECT",
+            "SPEAK", 
+            "MUTE_MEMBERS", 
+            "DEAFEN_MEMBERS", 
+            "MOVE_MEMBERS",
+          ],
+        }
+      ]
     });
     
     // Add voice channel to redis server
