5c1260e3733c077dac77f5e334c453243d5b188f
itsbadr <68023752+clickykeyboard@users.noreply.github.com>
2022-03-14 11:47:46 +0500

    prototype for vc name change

--- commands/name.ts
+++ commands/name.ts
@@ -1,11 +1,24 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, GuildMember } from "discord.js";
+import { getVoiceChannelFromHash } from "../utils/vc";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName("slash")
-    .setDescription("Slash command"),
+    .setName("name")
+    .setDescription("Rename your voice channel"),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply("Hello world");
+    const member = interaction.member as GuildMember;
+    const guild = interaction.guild;
+
+    getVoiceChannelFromHash(member.id)
+      .then(channelId => {
+        if (!channelId)
+          return interaction.reply("You are not in a voice channel");
+
+        const voiceChannel = guild?.channels.cache.get(channelId);
+        voiceChannel?.edit({ name: member.displayName })
+          .then(() => interaction.reply("Your voice channel has been renamed"));
+      })
+      .catch(error => console.log(error))
   }
 }
