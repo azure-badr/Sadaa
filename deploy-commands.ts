b2f954601029d052c845984d21a2a12426995821
itsbadr <68023752+clickykeyboard@users.noreply.github.com>
2022-03-14 11:27:43 +0500

    load slash commands from command dir

--- deploy-commands.ts
+++ deploy-commands.ts
@@ -1,19 +1,26 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
 import { REST } from "@discordjs/rest"
 import { Routes } from "discord-api-types/v9"
+import { readdirSync } from "fs";
+import path from "path";
 
 import { applicationId, guildId, token } from "./config.json";
+import SlashCommandObject from "./models/SlashCommandObject";
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("slash")
-    .setDescription("Slash command")
-]
-  .map(command => command.toJSON())
+const commandFiles = readdirSync(path.join(__dirname, "/commands/")).filter(file => file);
+const commands: Array<SlashCommandBuilder> = [];
+
+commandFiles.forEach(file => {
+  const { data: command } = require(`./commands/${file}`).default as SlashCommandObject;
+  commands.push(command);
+})
 
 const rest = new REST({ version: '9' })
   .setToken(token);
 
-rest.put(Routes.applicationGuildCommands(applicationId, guildId), { body: commands })
+rest.put(Routes.applicationGuildCommands(applicationId, guildId),
+  {
+    body: commands.map(command => command.toJSON())
+  })
   .then(() => console.log("Successfully deployed commands"))
   .catch(console.error);
