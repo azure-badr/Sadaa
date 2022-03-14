8c499f9ce4a05b351f15429e54d1cbad314051f5
itsbadr <68023752+clickykeyboard@users.noreply.github.com>
2022-03-14 19:45:26 +0500

    add lock / unlock

---
+++ commands/lock.ts
@@ -0,0 +1,30 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { CommandInteraction, GuildMember, Role, VoiceChannel } from "discord.js";
+import { getVoiceChannelFromHash, saveVoiceChannel } from "../utils/vc";
+
+export default {
+  data: new SlashCommandBuilder()
+    .setName("lock")
+    .setDescription("Locks the voice channel"),
+  async execute(interaction: CommandInteraction) {
+    const member = interaction.member as GuildMember;
+    const guild = interaction.guild;
+
+    getVoiceChannelFromHash(member.id)
+      .then(channelId => {
+        if (!channelId)
+          return interaction.reply("You are not in a voice channel");
+
+        const voiceChannel = guild?.channels.cache.get(channelId) as VoiceChannel;
+
+        voiceChannel.permissionOverwrites.edit(guild?.roles.everyone as Role, {
+          CONNECT: false
+        })
+        .then(async () => {
+            await saveVoiceChannel(voiceChannel.id);
+            interaction.reply("The voice channel has been locked");
+          });
+      })
+      .catch(error => console.log(error))
+  }
+}
