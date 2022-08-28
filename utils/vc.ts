fb3bc629e2bd390a0bad447f0fb380e291ac97e1
Badr <badrkmc@gmail.com>
2022-08-29 00:05:58 +0500

    Update vc.ts

--- utils/vc.ts
+++ utils/vc.ts
@@ -3,7 +3,9 @@ import { createClient } from "redis";
 
 import { applicationId } from "../config.json";
 
-const client = createClient();
+const client = createClient({
+  url: `redis://default:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${REDIS_PORT}`
+});
 client.connect()
   .then(() => console.log("Redis client connected"));
 
