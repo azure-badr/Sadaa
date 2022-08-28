10d1625638dc9e17e66c0df78da268c61c3d72ac
Badr <badrkmc@gmail.com>
2022-08-29 00:26:27 +0500

    any type for config

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
-export const applicationId: string | undefined = process.env.APPLICATION_ID;
-export const guildId: string | undefined = process.env.GUILD_ID;
-export const token: string | undefined = process.env.BOT_TOKEN;
-export const voiceChannelId: string | undefined = process.env.VOICE_CHANNEL_ID;
-export const categoryId: string | undefined = process.env.CATEGORY_ID;
-export const idleCategoryId: string | undefined = process.env.IDLE_CATEGORY_ID;
-export const defaultUserLimit: Number | undefined = 5 || process.env.DEFAULT_USER_LIMIT;
-export const defaultBitrate: Number | undefined = 96000 || process.env.DEFAULT_BITRATE;
+export const applicationId: any = process.env.APPLICATION_ID;
+export const guildId: any = process.env.GUILD_ID;
+export const token: any = process.env.BOT_TOKEN;
+export const voiceChannelId: any = process.env.VOICE_CHANNEL_ID;
+export const categoryId: any = process.env.CATEGORY_ID;
+export const idleCategoryId: any = process.env.IDLE_CATEGORY_ID;
+export const defaultUserLimit: any = 5 || process.env.DEFAULT_USER_LIMIT;
+export const defaultBitrate: any = 96000 || process.env.DEFAULT_BITRATE;
