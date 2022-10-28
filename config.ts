f67ba636652f22ec8f7fee1003c7b326c141ce9d
Badr <badrkmc@gmail.com>
2022-10-28 12:06:08 +0500

    set perms to view channel for mehmaan if changed

--- config.ts
+++ config.ts
@@ -2,6 +2,7 @@ export const applicationId: any = process.env.APPLICATION_ID;
 export const guildId: any = process.env.GUILD_ID;
 export const token: any = process.env.BOT_TOKEN;
 export const voiceChannelId: any = process.env.VOICE_CHANNEL_ID;
+export const mehmaanChannelId: any = process.env.MEHMAAN_CHANNEL_ID;
 export const categoryId: any = process.env.CATEGORY_ID;
 export const idleCategoryId: any = process.env.IDLE_CATEGORY_ID;
 export const defaultUserLimit: any = 5 || process.env.DEFAULT_USER_LIMIT;
---
+++ events/channelUpdate.ts
@@ -0,0 +1,18 @@
+import { PermissionFlagsBits } from "discord-api-types/v9"
+import { GuildChannel } from "discord.js"
+import { mehmaanChannelId } from "../config"
+
+export default {
+  name: "channelUpdate",
+  execute: async (oldChannel: GuildChannel, newChannel: GuildChannel): Promise<void> => {
+    // Check if permissions for Mehmaan channel is changed, then revert it
+    const newMehmaanPermissionOverwrites =
+      newChannel.permissionOverwrites.cache.get(mehmaanChannelId);
+    
+    if (!newMehmaanPermissionOverwrites?.allow.has(PermissionFlagsBits.ViewChannel)) {
+      await newChannel.permissionOverwrites.edit(mehmaanChannelId, {
+        VIEW_CHANNEL: true
+      });
+    }
+  },
+}
