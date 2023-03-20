50246f956554b824c99ad8942e1408929f777ed0
Badr <badrkmc@gmail.com>
2023-03-20 13:06:56 +0500

    add prototype for context menu interactions

---
+++ context_menus/kick.ts
@@ -0,0 +1,11 @@
+import { ContextMenuCommandBuilder } from "@discordjs/builders";
+import { ContextMenuInteraction } from "discord.js";
+
+export default {
+  data: new ContextMenuCommandBuilder()
+    .setName("kick")
+    .setType(2), // 2 = USER (@TODO: Types not being resolved)
+  async execute(interaction: ContextMenuInteraction) {
+    console.log("Kicking user");
+  }
+}
