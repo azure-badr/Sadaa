34a5144d7543229fc5d9a75b681b9321ed80eb24
Badr <badrkmc@gmail.com>
2023-03-21 18:01:53 +0500

    set default permissions as none

--- context_menus/allow.ts
+++ context_menus/allow.ts
@@ -4,6 +4,7 @@ import { ContextMenuInteraction } from "discord.js";
 export default {
   data: new ContextMenuCommandBuilder()
     .setName("allow")
+    .setDefaultPermission(false)
     .setType(2), // 2 = USER (@TODO: Types not being resolved)
   async execute(interaction: ContextMenuInteraction) {
     console.log("Allowing user");
--- context_menus/kick.ts
+++ context_menus/kick.ts
@@ -4,6 +4,7 @@ import { ContextMenuInteraction } from "discord.js";
 export default {
   data: new ContextMenuCommandBuilder()
     .setName("kick")
+    .setDefaultPermission(false)
     .setType(2), // 2 = USER (@TODO: Types not being resolved)
   async execute(interaction: ContextMenuInteraction) {
     console.log("Kicking user");
