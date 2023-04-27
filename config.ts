b0b2fabeed8032f543522ac0eeb13559b78e47a8
Badr <badrkmc@gmail.com>
2023-04-27 15:49:17 +0500

    update config to include name allowed role ID

--- config.ts
+++ config.ts
@@ -1,11 +1,12 @@
 export const applicationId: any = process.env.APPLICATION_ID;
 export const guildId: any = process.env.GUILD_ID;
-export const modRoleId: any = process.env.MOD_ROLE_ID
+export const nameChangeAllowedRoleId: any = process.env.NAME_CHANGE_ALLOWED_ROLE_ID;
 export const token: any = process.env.BOT_TOKEN;
 export const voiceChannelId: any = process.env.VOICE_CHANNEL_ID;
 export const mehmaanChannelId: any = process.env.MEHMAAN_CHANNEL_ID;
 export const categoryId: any = process.env.CATEGORY_ID;
 export const idleCategoryId: any = process.env.IDLE_CATEGORY_ID;
+export const modRoleId: any = process.env.MOD_ROLE_ID
 export const defaultUserLimit: any = process.env.DEFAULT_USER_LIMIT || 5;
 export const defaultBitrate: any = process.env.DEFAULT_BITRATE || 96000;
 export const redisPassword: any = process.env.REDISPASSWORD;
