0c49a972fc06cd8a8087994372ddad648d9d52a7
Wookie <bq1214@gmail.com>
2024-07-06 14:27:08 +0500

    feat: use OpenAI moderation for channel names

--- events/modalHandle.ts
+++ events/modalHandle.ts
@@ -1,4 +1,5 @@
 import { ModalSubmitInteraction } from "discord.js";
+import { moderateTerm } from "../utils/ai";
 
 export default {
   async execute(interaction: ModalSubmitInteraction) {
@@ -9,6 +10,9 @@ export default {
 
     const nameInput = interaction.fields.getTextInputValue("sadaa-name-input");
     if (!nameInput) return interaction.reply("An error occured while renaming your voice channel");
+
+    const isTermSafe = await moderateTerm(nameInput);
+    if (!isTermSafe) return interaction.reply("That term is not allowed");
 
     await channel.setName(nameInput);
     await interaction.reply("Your voice channel has been renamed ✨");
