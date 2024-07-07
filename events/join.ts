40672626139be68fb5bc180333ec43e37e3061a3
Wookie <bq1214@gmail.com>
2024-07-07 17:15:26 +0500

    fix: added vc name censoring (#9)

--- events/join.ts
+++ events/join.ts
@@ -1,23 +1,25 @@
-import { CategoryChannel, ChannelType, DiscordAPIError, DiscordErrorData, GuildMember, VoiceChannel, VoiceState } from "discord.js"
-
 import {
-  voiceChannelId,
-  categoryId,
-  modRoleId,
-  defaultUserLimit,
-  defaultBitrate
-} from "../config";
+  CategoryChannel,
+  ChannelType,
+  DiscordAPIError,
+  GuildMember,
+  VoiceChannel,
+  VoiceState,
+  PermissionFlagsBits,
+} from "discord.js";
+
+import { voiceChannelId, categoryId, modRoleId, defaultUserLimit, defaultBitrate } from "../config";
 import { addActiveVoiceChannel, isVoiceChannelSaved } from "../utils/vc";
-import { PermissionFlagsBits } from "discord.js";
 
 async function createVoiceChannel(voiceState: VoiceState, name?: string) {
   const guild = voiceState.guild;
   const member = voiceState.member as GuildMember;
 
-  try {
-    const channelCategory = guild.channels.cache.get(categoryId) as CategoryChannel;
+  const channelCategory = guild.channels.cache.get(categoryId) as CategoryChannel;
 
-    const memberChannel = await guild.channels.create({
+  // removed the try...catch block since the error gets thrown either way (at least it should) and should be caught by the calling function
+  const memberChannel = await guild.channels
+    .create({
       name: name || `${member?.displayName}'s VC`,
       type: ChannelType.GuildVoice,
       bitrate: defaultBitrate,
@@ -39,14 +41,40 @@ async function createVoiceChannel(voiceState: VoiceState, name?: string) {
         },
       ],
       parent: channelCategory,
-    });
-    
-    addActiveVoiceChannel(member?.id, memberChannel.id);
-    return memberChannel;
+    })
+    .catch(() => {
+      // create a censored name from the user's display name
+      const censored = member?.displayName.replace(/./g, "*");
 
-  } catch (error: any) {
-    throw error;
-  }
+      // if this fails, then it'll be caught in the error block below where I commented and it means the error was something else
+      return guild.channels.create({
+        name: name || `${censored}'s VC`,
+        type: ChannelType.GuildVoice,
+        bitrate: defaultBitrate,
+        userLimit: defaultUserLimit,
+        permissionOverwrites: [
+          {
+            id: member?.id,
+            allow: [
+              PermissionFlagsBits.Connect,
+              PermissionFlagsBits.Speak,
+              PermissionFlagsBits.MuteMembers,
+              PermissionFlagsBits.DeafenMembers,
+              PermissionFlagsBits.MoveMembers,
+            ],
+          },
+          {
+            id: modRoleId,
+            allow: [PermissionFlagsBits.ManageChannels],
+          },
+        ],
+        parent: channelCategory,
+      });
+    });
+
+  addActiveVoiceChannel(member?.id, memberChannel.id);
+
+  return memberChannel;
 }
 
 async function moveToActiveCategory(memberId: string, voiceChannel: VoiceChannel): Promise<VoiceChannel> {
@@ -54,7 +82,7 @@ async function moveToActiveCategory(memberId: string, voiceChannel: VoiceChannel
   const activeCategory = guild.channels.cache.get(categoryId) as CategoryChannel;
 
   await voiceChannel.setParent(activeCategory, { lockPermissions: false });
-  await voiceChannel.permissionOverwrites.edit(guild.roles.everyone, { ViewChannel: true })
+  await voiceChannel.permissionOverwrites.edit(guild.roles.everyone, { ViewChannel: true });
 
   addActiveVoiceChannel(memberId, voiceChannel.id);
 
@@ -64,32 +92,32 @@ async function moveToActiveCategory(memberId: string, voiceChannel: VoiceChannel
 export default {
   name: "voiceStateUpdate",
   once: false,
-  execute: async (oldState: VoiceState, newState: VoiceState) => { 
+  execute: async (oldState: VoiceState, newState: VoiceState) => {
     if (oldState.channel) return;
-    
+
     if (newState.channelId != voiceChannelId) return;
 
     const member = newState.member;
-    console.log(`[Join] Member {${member?.displayName}} joined to create a VC`)
-    
+    console.log(`[Join] Member {${member?.displayName}} joined to create a VC`);
+
     const savedChannelId = await isVoiceChannelSaved(member as GuildMember);
     if (savedChannelId) {
       const voiceChannel = member?.guild?.channels.cache.get(savedChannelId as string) as VoiceChannel;
-      moveToActiveCategory(member?.id as string, voiceChannel)
-        .then((channel) => {
-          member?.voice
-            .setChannel(channel)
-            .catch(() => { return; })
-        })
+      moveToActiveCategory(member?.id as string, voiceChannel).then((channel) => {
+        member?.voice.setChannel(channel).catch(() => {
+          // this is where the error will be caught even if the censoring fails
+          return;
+        });
+      });
 
       return;
     }
 
     createVoiceChannel(newState)
-      .then(channel => {
-        member?.voice
-          .setChannel(channel)
-          .catch(() => { return; })
+      .then((channel) => {
+        member?.voice.setChannel(channel).catch(() => {
+          return;
+        });
       })
       .catch(async (error) => {
         if (!(error instanceof DiscordAPIError)) return;
@@ -98,12 +126,12 @@ export default {
         // Yeah, there's no way around this. This is how you do it..
         const errors = error.rawError as any;
         if (errors.errors.name._errors[0].code == "INVALID_COMMUNITY_PROPERTY_NAME") {
-          console.log(`[Join] Member {${member?.displayName}} tried to create a VC with a name that is not allowed.`)
+          console.log(`[Join] Member {${member?.displayName}} tried to create a VC with a name that is not allowed.`);
           const channel = await createVoiceChannel(newState, "Your VC");
-          member?.voice
-            .setChannel(channel)
-            .catch(() => { return; })
+          member?.voice.setChannel(channel).catch(() => {
+            return;
+          });
         }
       });
-  }
-}
+  },
+};
